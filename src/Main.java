6528612e275230c52ad7ce1602f9b3dfafc06679
atena kalaki <atena.kalaki@gmail.com>
2020-05-19 10:33:31 +1000

    Lambda Scope

--- src/Main.java
+++ src/Main.java
@@ -6,19 +6,19 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) {
-        new Thread( ()->System.out.println("Printing from the Runnable")
-         ).start();
+        new Thread(() -> System.out.println("Printing from the Runnable")
+        ).start();
 
-    Employee john = new Employee("john doe",21);
-    Employee john2 = new Employee("john2 doe",22);
-    Employee john3 = new Employee("john3 doe",23);
-    Employee john4 = new Employee("john4 doe",24);
+        Employee john = new Employee("john doe", 21);
+        Employee john2 = new Employee("john2 doe", 22);
+        Employee john3 = new Employee("john3 doe", 23);
+        Employee john4 = new Employee("john4 doe", 24);
 
-    List<Employee> employeeList = new ArrayList<>();
-    employeeList.add(john);
-    employeeList.add(john2);
-    employeeList.add(john3);
-    employeeList.add(john4);
+        List<Employee> employeeList = new ArrayList<>();
+        employeeList.add(john);
+        employeeList.add(john2);
+        employeeList.add(john3);
+        employeeList.add(john4);
 
 //        Collections.sort(employeeList, new Comparator<Employee>() {
 //            @Override
@@ -27,15 +27,37 @@ public class Main {
 //            }
 //        });
 
-        Collections.sort(employeeList,( employee1, employee2)->
+        Collections.sort(employeeList, (employee1, employee2) ->
                 employee1.getName().compareTo(employee2.getName()));
 
-    for (Employee employee: employeeList){
-        System.out.println(employee.getName());
+        for (Employee employee : employeeList) {
+            System.out.println(employee.getName());
+        }
+
+//        String sillyString = doStringStuff(new UpperConcat() {
+//            @Override
+//            public String upperAndConcat(String s1, String s2) {
+//                return s1.toUpperCase() + s2.toUpperCase();
+//            }
+//        }, employeeList.get(0).getName(), employeeList.get(1).getName());
+
+        UpperConcat uc = (s1, s2) -> s1.toUpperCase() + s2.toUpperCase();
+        String sillyString = doStringStuff(uc,
+                employeeList.get(0).getName(), employeeList.get(1).getName());
+
+        System.out.println(sillyString);
+
+        AnotherClass anotherClass = new AnotherClass();
+        String s = anotherClass.doSomething();
+        System.out.println(s);
     }
 
+    public final static String doStringStuff(UpperConcat uc, String s1, String s2) {
+
+        return uc.upperAndConcat(s1, s2);
 
     }
+
 
 }
 //class CodeToRun implements  Runnable{
@@ -47,7 +69,7 @@ public class Main {
 //    }
 //}
 
-class Employee{
+class Employee {
     private String name;
     private int age;
 
@@ -72,3 +94,32 @@ class Employee{
         this.age = age;
     }
 }
+
+interface UpperConcat {
+    public String upperAndConcat(String s1, String s2);
+}
+
+class AnotherClass {
+//        public String doSomething(){
+//            System.out.println("The AnotherClass Name is "+ this.getClass().getSimpleName());
+//            return  Main.doStringStuff(new UpperConcat() {
+//                @Override
+//                public String upperAndConcat(String s1, String s2) {
+//                    System.out.println("The annonymous class Name is "+ this.getClass().getSimpleName());
+//
+//                    return s1.toUpperCase()+s2.toUpperCase();
+//                }
+//            }, "String1", "String2");
+//        }
+
+    public String doSomething() {
+        System.out.println("The AnotherClass Name is " + this.getClass().getSimpleName());
+        UpperConcat uc = (s1, s2) -> {
+            System.out.println("The annonymous class Name is " + this.getClass().getSimpleName());
+            return s1.toUpperCase() + s2.toUpperCase();
+        };
+        return Main.doStringStuff(uc, "String1", "String2");
+    }
+
+}
+
